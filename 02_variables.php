870e843a65adc12e98de4679c914db56183ea6b4
Trugtie <nhamtrungtien2013@gmail.com>
2021-10-07 21:08:57 +0700

    Chapter: Number!

--- 02_variables.php
+++ 02_variables.php
@@ -1,25 +1,64 @@
 <?php
 
-// What is a variable
+// What is a variable: Store data
 
 // Variable types
+/* 
+String
+Integer
+Float/Double
+Boolean
+Null
+Array
+Object
+Resource
+*/
 
 // Declare variables
+$name="Zura";
+$age=22;
+$isMale=true; // if false when print out empty string
+$height=1.85;
+$salary=null; // print out empty string
 
 // Print the variables. Explain what is concatenation
+echo $name."<br>";
+echo $age."<br>";
+echo $isMale."<br>";
+echo $height."<br>";
+echo $salary."<br>";
 
 // Print types of the variables
+echo gettype($name)."<br>";
+echo gettype($age)."<br>";
+echo gettype($isMale)."<br>";
+echo gettype($height)."<br>";
+echo gettype($salary)."<br>";
 
 // Print the whole variable
+echo var_dump($name,$age,$isMale,$height,$salary);
 
 // Change the value of the variable
+$name="false";
 
 // Print type of the variable
+echo gettype($name)."<br>";
 
 // Variable checking functions
+echo is_int($age)."<br>";
+echo is_string($name)."<br>";
+echo is_bool($isMale)."<br>";
 
 // Check if variable is defined
+echo isset($name)."<br>";
+echo isset($address)."<br>";
 
 // Constants
+define("PI",3.14);
+echo PI."<br>";
+define("Oneyear","1 year");
+echo Oneyear."<br>";
 
 // Using PHP built-in constants
+echo SORT_ASC."<br>";
+echo PHP_INT_MAX."<br>";
--- 03_numbers.php
+++ 03_numbers.php
@@ -1,21 +1,55 @@
 <?php
 
 // Declaring numbers
-
+$a=4;
+$b=5;
+$c=1.5;
 // Arithmetic operations
-
+echo $a + $b + $c . "<br>";
+echo $a + $b * $c . "<br>";
 // Assignment with math operators
+$a+=$b;
+echo $a . "<br>";
 
 // Increment operator
+echo $a++ . "<br>";
+echo ++$a . "<br>";
 
 // Decrement operator
+echo $a-- . "<br>";
+echo --$a . "<br>";
 
 // Number checking functions
 
+is_float($a);
+is_double($a);
+is_int($a);
+echo is_numeric("123")."<br>"; //true
+echo is_numeric("1a23")."<br>"; // false
+
 // Conversion
+$strNumber="12.34";
+$number=(float)$strNumber;
+echo var_dump($number) . "<br>";
+$number = strval($strNumber);
+echo var_dump($number) . "<br>";
 
 // Number functions
+echo abs(-15)."<br>";
+echo pow(2,2)."<br>";
+echo sqrt(4)."<br>";
+echo max(2,5,1)."<br>";
+echo min(2,4,5)."<br>";
+echo floor(2.6)."<br>"; //round down
+echo round(3.5)."<br>"; //round with math
+echo round(3.3)."<br>";
+echo ceil(1.2)."<br>"; //round up
 
 // Formatting numbers
+$number =123456789.123456;
+//Parameters for number_format
+// (variables, decimals(bao nhiêu số thập phân phía sau), decimals_separator(dấu ngăn số thập phân), thousands_separator(dấu ngăn đơn vị ngàn))    
+echo number_format($number,2,",",".");
+
 
 // https://www.php.net/manual/en/ref.math.php
