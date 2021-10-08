1c81e5525116b6e1cb5e67ce86e99b975e9a8868
Trugtie <nhamtrungtien2013@gmail.com>
2021-10-08 22:11:48 +0700

    Chapter Dates Continues

--- 08_functions.php
+++ 08_functions.php
@@ -1,43 +1,49 @@
 <?php
 
 // Function which prints "Hello I am Zura"
-function hello(){
-    echo "Hello I am Zura".'<br>';
+function hello()
+{
+    echo "Hello I am Zura" . '<br>';
 }
 hello();
 hello();
 hello();
 // Function with argument
-function hello1($name){
-    echo "Hello I am $name!".'<br>';
+function hello1($name)
+{
+    echo "Hello I am $name!" . '<br>';
 }
 hello1("Tien");
 // Create sum of two functions
-function sum($a,$b){
-    echo $a+$b.'<br>';
+function sum($a, $b)
+{
+    echo $a + $b . '<br>';
 }
-sum(1,2);
+sum(1, 2);
 // Create function to sum all numbers using ...$nums
-function sumAll($nums){
-    $sum=0;
-    foreach($nums as $num){
-        $sum+=$num;
+function sumAll($nums)
+{
+    $sum = 0;
+    foreach ($nums as $num) {
+        $sum += $num;
     }
     return $sum;
 }
-echo sumAll([1,2,3])."<br>";
+echo sumAll([1, 2, 3]) . "<br>";
 
-function sumAll1(...$nums){
-    $sum=0;
-    foreach($nums as $num){
-        $sum+=$num;
+function sumAll1(...$nums)
+{
+    $sum = 0;
+    foreach ($nums as $num) {
+        $sum += $num;
     }
     return $sum;
 }
-echo sumAll1(2,3,4)."<br>";
+echo sumAll1(2, 3, 4) . "<br>";
 // Arrow functions
-function sumArrow(...$nums){
-    return array_reduce($nums,fn($carry,$n)=>$carry + $n);
+function sumArrow(...$nums)
+{
+    return array_reduce($nums, fn ($carry, $n) => $carry + $n);
 }
 
-echo sumArrow(5,6,7);
+echo sumArrow(5, 6, 7);
