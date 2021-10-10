7a74cba534584bf238845a2df960c7202bb7eb02
Trugtie <nhamtrungtien2013@gmail.com>
2021-10-10 22:05:13 +0700

    Create, Edit, Delete basic

--- 14_product_crud/index.php
+++ 14_product_crud/index.php
@@ -2,8 +2,15 @@
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+$search=$_GET['search']??'';
+if($search){
+    $statement = $pdo->prepare('select * from products where title like :title order by create_date desc');
+    $statement->bindValue(':title',"%$search%");
+}else{
+    $statement = $pdo->prepare('select * from products order by create_date desc');
+}
 
-$statement = $pdo->prepare('select * from products order by create_date desc');
+
 $statement->execute();
 $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 // echo '<pre>';
@@ -33,6 +40,13 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
     <p>
         <a href="./create.php" class="btn btn-success">Create Product</a>
     </p>
+
+   <form action="">
+   <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Search for products" name="search" value="<?php echo $search; ?>">
+        <button class="btn btn-outline-secondary" type="submit">Search</button>
+    </div>
+   </form>
     <table class="table">
         <thead>
             <tr>
@@ -49,14 +63,18 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
                 <tr>
                     <th scope="row"><?php echo $i + 1 ?></th>
                     <td>
-                        <img class="thumb-image" src="<?php echo $product['image']?>" alt="">
+                        <img class="thumb-image" src="<?php echo $product['image'] ?>" alt="">
                     </td>
                     <td><?php echo $product['title'] ?></td>
                     <td>@<?php echo $product['price'] ?></td>
                     <td>@<?php echo $product['create_date'] ?></td>
                     <td>
-                        <button type="button" class="btn btn-sm btn-primary">Edit</button>
-                        <button type="button" class="btn btn-sm btn-danger">Delete</button>
+                        <a href="./update.php?id=<?php echo $product['id'] ?>" class="btn btn-sm btn-primary">Edit</a>
+                        <form style="display: inline-block" action="./delete.php" method="post">
+                            <input type="hidden" name="id" value="<?php echo $product['id'] ?>">
+                            <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                        </form>
+
                     </td>
                 </tr>
             <?php endforeach; ?>
