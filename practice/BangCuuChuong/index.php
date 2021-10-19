a134999f5bd9b6740e1009fdeb61220caf1156c4
Trugtie <nhamtrungtien2013@gmail.com>
2021-10-19 11:18:13 +0700

    Fix

--- practice/BangCuuChuong/index.php
+++ practice/BangCuuChuong/index.php
@@ -46,6 +46,27 @@ function randomColor()
             ?>
         </div>
     </section>
+    <section>
+        <!-- Cho một mảng một chiều  : Hãy tìm số lớn nhât, số nhỏ nhât và sắp xếp  các số theo thứ tự tăng dần. -->
+        <header>
+            <h1>MẢNG MỘT CHIỀU</h1>
+        </header>
+        <div class="container" style="display:flex; justify-content:center;">
+            <div class='cuuchuong-container' style="max-width: 100rem;width:100%;">
+                <?php
+                $array = [2, 5, 6, 8, 1];
+                $arrayStr = implode(",", $array);
+                echo "Mảng một chiều: $arrayStr";
+                echo $max = max($array) . "<br>";
+                echo $min = min($array) . "<br>";
+                $array = explode(",", $arrayStr);
+                sort($array);
+                $arrayStr = implode(",", $array);
+                echo "Mảng một chiều theo thứ tự tăng dần: $arrayStr";
+                ?>
+            </div>
+        </div>
+    </section>
     <footer>
         Author: Trugtie
     </footer>
